80bf6711acec41ef2bbb8e051cb1678c7cee8ba6
Rafael A. Fernandez Morera <rfmorera@gmail.com>
2019-03-31 19:31:37 -0400

    Migration Order property added

---
+++ Data/Migrations/20190331233011_ModelsOrdenAdded.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlueDot.Data.Migrations
+{
+    public partial class ModelsOrdenAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Orden",
+                table: "Grupo",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Orden",
+                table: "Categoria",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Orden",
+                table: "Anuncio",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Orden",
+                table: "Grupo");
+
+            migrationBuilder.DropColumn(
+                name: "Orden",
+                table: "Categoria");
+
+            migrationBuilder.DropColumn(
+                name: "Orden",
+                table: "Anuncio");
+        }
+    }
+}
