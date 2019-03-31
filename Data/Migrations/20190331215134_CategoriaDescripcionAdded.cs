c192ad5d87210253e5bbbb3ec05e2ccde9377eb3
Rafael A. Fernandez Morera <rfmorera@gmail.com>
2019-03-31 19:01:09 -0400

    Descripcion added to Categoria

---
+++ Data/Migrations/20190331215134_CategoriaDescripcionAdded.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlueDot.Data.Migrations
+{
+    public partial class CategoriaDescripcionAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Categoria",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Descripcion",
+                table: "Categoria",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Descripcion",
+                table: "Categoria");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Categoria",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
