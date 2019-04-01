28894fd5a5261524a61bf10aef8efede1e138b14
Rafael A. Fernandez Morera <rfmorera@gmail.com>
2019-03-31 20:46:20 -0400

    Activado property added

---
+++ Data/Migrations/20190401004216_GrupoActivadoFlag.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlueDot.Data.Migrations
+{
+    public partial class GrupoActivadoFlag : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Activo",
+                table: "Grupo",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Activo",
+                table: "Grupo");
+        }
+    }
+}
