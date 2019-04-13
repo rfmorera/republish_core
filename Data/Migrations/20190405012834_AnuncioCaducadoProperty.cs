f46e75432639dfcf87f4e624bbb798d1c8d3f025
Rafael A. Fernandez Morera <rfmorera@gmail.com>
2019-04-13 13:23:16 -0400

    Migrations Temporizador y Anuncio Caducado

---
+++ Data/Migrations/20190405012834_AnuncioCaducadoProperty.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlueDot.Data.Migrations
+{
+    public partial class AnuncioCaducadoProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "Caducado",
+                table: "Anuncio",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldDefaultValue: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "Caducado",
+                table: "Anuncio",
+                nullable: false,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldDefaultValue: false);
+        }
+    }
+}
---
+++ Data/Migrations/20190413032334_TemporizadorNextExecution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlueDot.Data.Migrations
+{
+    public partial class TemporizadorNextExecution : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "NextExecution",
+                table: "Temporizador",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NextExecution",
+                table: "Temporizador");
+        }
+    }
+}
