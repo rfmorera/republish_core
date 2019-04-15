5cbf8b2304ed3126f2ab927abf614deb56d0200e
Rafael A. Fernandez Morera <rfmorera@gmail.com>
2019-04-15 19:04:19 -0400

    AutoLauncher working

--- AutoLauncher/Function1.cs
+++ AutoLauncher/Function1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 
@@ -7,9 +8,29 @@ namespace AutoLauncher
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static void Run([TimerTrigger("0 1 * * * *")]TimerInfo myTimer, TraceWriter log)
+        public static void Run([TimerTrigger("0 * * * * *")]TimerInfo myTimer, TraceWriter log/*, IChequerService chequerService*/)
         {
-            log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
+            try
+            {
+                log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
+                WebRequest request = WebRequest.Create("https://localhost:44306/Client/Grupo/CheckTemporizadores");
+                //request.Timeout = (int)TimeSpan.FromMinutes(5).TotalMilliseconds;
+                ((HttpWebRequest)request).UserAgent = ".NET Framework Client";
+                request.Credentials = CredentialCache.DefaultCredentials;
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    log.Info($"Chequeo Ok!!");
+                }
+                else
+                {
+                    log.Info($"~~~ Bad ~~~ Chequeo incorrecto");
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Info(ex.Message);
+            }
         }
     }
 }
