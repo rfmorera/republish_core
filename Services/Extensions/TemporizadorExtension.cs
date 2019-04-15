a81335c54b26a7d0016f25d93e9c33351458b137
Rafael A. Fernandez Morera <rfmorera@gmail.com>
2019-04-15 19:05:13 -0400

    Temporizador extension

---
+++ Services/Extensions/TemporizadorExtension.cs
@@ -0,0 +1,33 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Extensions
+{
+    public static class TemporizadorExtension
+    {
+        public static bool IsValidDay(this Temporizador value)
+        {
+            DayOfWeek today = DateTime.Now.DayOfWeek;
+            switch (today)
+            {
+                case DayOfWeek.Sunday:
+                    return value.Domingo;
+                case DayOfWeek.Monday:
+                    return value.Lunes;
+                case DayOfWeek.Tuesday:
+                    return value.Martes;
+                case DayOfWeek.Wednesday:
+                    return value.Miercoles;
+                case DayOfWeek.Thursday:
+                    return value.Jueves;
+                case DayOfWeek.Friday:
+                    return value.Viernes;
+                case DayOfWeek.Saturday:
+                    return value.Sabado;
+            }
+            throw new Exception("Invalid day of week");
+        }
+    }
+}
