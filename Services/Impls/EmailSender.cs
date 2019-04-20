fe73e429c01d2b41b3f614ea07a9b58ce6827e2c
Rafael A. Fernandez Morera <rfmorera@gmail.com>
2019-04-20 16:49:39 -0400

    manual set email configuratin

--- Services/Impls/EmailSender.cs
+++ Services/Impls/EmailSender.cs
@@ -23,17 +23,31 @@ namespace Services.Impls
 
         public async Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
+            //Company company = _context.Set<Company>().Single();
+
+            //SmtpClient client = new SmtpClient(company.ExternalName, company.ExternalPort);
+            //client.Timeout = company.ExternalTimeout;
+            //System.Net.NetworkCredential credentials = new System.Net.NetworkCredential(company.ExternalUsername, company.ExternalPassword);
+            //client.Credentials = credentials;
+
+            //string content = _emailTemplate.Template;
+            //content = content.Replace("{EMAIL_CONTENT}", htmlMessage);
+
+            //MailMessage mail = new MailMessage(company.FromEmail, email);
+            //mail.Subject = subject;
+            //mail.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(content, null, MediaTypeNames.Text.Html));
+
             Company company = _context.Set<Company>().Single();
 
-            SmtpClient client = new SmtpClient(company.ExternalName, company.ExternalPort);
+            SmtpClient client = new SmtpClient("http://republishtool.com");
             client.Timeout = company.ExternalTimeout;
-            System.Net.NetworkCredential credentials = new System.Net.NetworkCredential(company.ExternalUsername, company.ExternalPassword);
+            System.Net.NetworkCredential credentials = new System.Net.NetworkCredential("info", "ocvXkyfseq$Npu?in2hd1tbjmrglzx");
             client.Credentials = credentials;
 
             string content = _emailTemplate.Template;
             content = content.Replace("{EMAIL_CONTENT}", htmlMessage);
 
-            MailMessage mail = new MailMessage(company.FromEmail, email);
+            MailMessage mail = new MailMessage("info@republishtool.com", email);
             mail.Subject = subject;
             mail.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(content, null, MediaTypeNames.Text.Html));
 
