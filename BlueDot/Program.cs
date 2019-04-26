9e1a3874ec6716488c3dc318d980e234b9269ec2
Rafael A. Fernandez Morera <rfmorera@gmail.com>
2019-04-25 23:05:47 -0400

    Service Point and ThreadPool configured

--- BlueDot/Program.cs
+++ BlueDot/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -12,8 +14,30 @@ namespace Republish
 {
     public class Program
     {
+        
         public static void Main(string[] args)
         {
+            ServicePoint sp1, sp2;
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(10, 10);
+            
+            // Upper Limit of Connections
+            ServicePointManager.DefaultConnectionLimit = 50;
+            ServicePointManager.MaxServicePoints = 25;
+            Uri uriRevolico = new Uri("https://www.revolico.com");
+            Uri uri2Captcha = new Uri("https://www.2captcha.com");
+
+            sp1 = ServicePointManager.FindServicePoint(uriRevolico);
+            sp2 = ServicePointManager.FindServicePoint(uri2Captcha);
+
+            // Configuration ServicePoint Revolico
+            sp1.ConnectionLimit = 100;
+            sp1.ConnectionLeaseTimeout = 6000;
+
+            // Configuration ServicePoint 2Captcha
+            sp2.ConnectionLimit = 100;
+            sp2.ConnectionLeaseTimeout = 6000;
+
             CreateWebHostBuilder(args).Build().Run();
         }
 
