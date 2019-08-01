4e05e589729c628420d2315855e5469a87506b76
Rafael A. Fernandez Morera <rfmorera@gmail.com>
2019-08-01 19:28:02 -0400

    CaptchaService implented

---
+++ Services/Impls/CaptchaService.cs
@@ -0,0 +1,41 @@
+﻿using Models;
+using Republish.Data;
+using Republish.Data.Repositories;
+using Republish.Data.RepositoriesInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Impls
+{
+    public class CaptchaService : ICaptchaService
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly IRepository<CaptchaKeys> repositoryCaptcha;
+
+        public CaptchaService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            repositoryCaptcha = new Repository<CaptchaKeys>(dbContext);
+        }
+
+        public async Task<CaptchaKeys> GetCaptchaKeyAsync()
+        {
+            return (await repositoryCaptcha.GetAllAsync()).First();
+        }
+
+        public async Task<CaptchaKeys> Update2CaptchaKey(string key)
+        {
+            CaptchaKeys captcha = await GetCaptchaKeyAsync();
+            await repositoryCaptcha.DeleteAsync(captcha);
+
+            captcha = new CaptchaKeys(key);
+
+            await repositoryCaptcha.AddAsync(captcha);
+
+            return captcha;
+        }
+    }
+}
