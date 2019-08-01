a63e29378c3b2a2a3c8fac41fa8560dc8431fcde
Rafael A. Fernandez Morera <rfmorera@gmail.com>
2019-08-01 19:27:52 -0400

    CaptchaKey Model and Migrations

---
+++ Models/CaptchaKeys.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Models
+{
+    public class CaptchaKeys
+    {
+        public CaptchaKeys(string key)
+        {
+            Id = key;
+        }
+
+        [Key]
+        [Required]
+        public string Id { get; set; }
+    }
+}
