3cffdf5a1e7ccd65057085d6ed85916bd44899c7
Rafael A. Fernandez Morera <rfmorera@gmail.com>
2019-08-02 14:46:23 -0400

    Roles const values created

---
+++ Models/RTRoles.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public static class RTRoles
+    {
+        public const string Admin = "Admin";
+        public const string Client = "Client";
+    }
+}
