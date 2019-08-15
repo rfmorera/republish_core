3ae0f706b50935c25c3f9de7007cf35b325d613a
Rafael A. Fernandez Morera <rfmorera@gmail.com>
2019-08-14 22:50:46 -0400

    Recarga y Saldo visual implementado

--- BlueDot/Areas/Client/Controllers/DefaultController.cs
+++ BlueDot/Areas/Client/Controllers/DefaultController.cs
@@ -15,19 +15,19 @@ namespace RepublishTool.Areas.Client.Controllers
     public class DefaultController : Controller
     {
         private readonly IRegistroService _registroService;
-        private readonly IEstadisticasService _estadisticasService;
+        private readonly IUserControlService _userControlService;
         private readonly UserManager<IdentityUser> _userManager;
-        public DefaultController(UserManager<IdentityUser> userManager, IRegistroService registroService, IEstadisticasService estadisticasService)
+        public DefaultController(UserManager<IdentityUser> userManager, IRegistroService registroService, IUserControlService userControlService)
         {
             _registroService = registroService;
             _userManager = userManager;
-            _estadisticasService = estadisticasService;
+            _userControlService = userControlService;
         }
 
         public async Task<IActionResult> Index()
         {
             IdentityUser user = await _userManager.GetUserAsync(User);
-            ClientDashboard dashboard = await _estadisticasService.GetDashboard(user);
+            ClientDashboard dashboard = await _userControlService.GetDashboard(user);
             return View(dashboard);
         }
     }
