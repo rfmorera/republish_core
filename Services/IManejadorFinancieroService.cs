41abc9694e4c1515cdecf64d470a4d961e4d6b09
Rafael A. Fernandez Morera <rfmorera@gmail.com>
2019-08-14 22:49:38 -0400

    Servicio de Manejo Financiero implementado

---
+++ Services/IManejadorFinancieroService.cs
@@ -0,0 +1,15 @@
+﻿using Services.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public interface IManejadorFinancieroService
+    {
+        Task InicializarUsuario(string UserId);
+        Task RecargarUsuario(RecargaDTO recargaDTO);
+        Task<double> GetSaldo(string UserId);
+    }
+}
