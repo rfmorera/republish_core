0742ecbace472c38a1d484e242516f459ecb5b6f
Rafael A. Fernandez Morera <rfmorera@gmail.com>
2019-08-20 00:04:56 -0400

    Exception detailed

---
+++ Services/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Extensions
+{
+    public static class ExceptionExtensions
+    {
+        public static string ToExceptionString(this Exception ex)
+        {
+            return ex.Source + "--\n--" + ex.StackTrace + "--\n--" + ex.Message;
+        }
+    }
+}
