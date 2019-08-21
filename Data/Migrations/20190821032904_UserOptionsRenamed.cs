e96cd1e5613ce7e8e91ea17c599a08561a52adf8
Rafael A. Fernandez Morera <rfmorera@gmail.com>
2019-08-21 09:22:09 -0400

    ClienteOpciones renombrada

---
+++ Data/Migrations/20190821032904_UserOptionsRenamed.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlueDot.Data.Migrations
+{
+    public partial class UserOptionsRenamed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TemporizadoresSystemEnable",
+                table: "ClienteOpciones",
+                newName: "TemporizadoresUserEnable");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TemporizadoresUserEnable",
+                table: "ClienteOpciones",
+                newName: "TemporizadoresSystemEnable");
+        }
+    }
+}
