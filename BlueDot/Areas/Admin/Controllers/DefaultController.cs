ff4eff49c7a146ca87bbf3ca31936a805b98afd7
Rafael A. Fernandez Morera <rfmorera@gmail.com>
2019-08-21 20:14:33 -0400

    Init Users

--- BlueDot/Areas/Admin/Controllers/DefaultController.cs
+++ BlueDot/Areas/Admin/Controllers/DefaultController.cs
@@ -3,18 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Models;
+using Republish.Data;
 
 namespace Republish.Areas.Admin.Controllers
 {
     [Area(RTRoles.Admin)]
-    [Authorize(Roles = RTRoles.Admin)]
+    
     public class DefaultController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ApplicationDbContext _dbContext;
+        public DefaultController(UserManager<IdentityUser> userManager, ApplicationDbContext dbContext)
+        {
+            _userManager = userManager;
+            _dbContext = dbContext;
+        }
+
+        [Authorize(Roles = RTRoles.Admin)]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> Init()
+        {
+            await _dbContext.Roles.AddAsync(new IdentityRole(RTRoles.Admin));
+            await _dbContext.Roles.AddAsync(new IdentityRole(RTRoles.Client));
+
+            IdentityUser raf = new IdentityUser("rfmorera@gmail.com");
+            await _userManager.CreateAsync(raf, "Ciber*2019");
+
+            await _userManager.AddToRoleAsync(raf, RTRoles.Admin);
+
+            return Ok();
         }
     }
 }
