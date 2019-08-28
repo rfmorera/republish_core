c1f2ea0501ac65fdf438ce39eec74680c0d7e85e
Rafael A. Fernandez Morera <rfmorera@gmail.com>
2019-08-28 00:43:45 -0400

    Agent View

---
+++ BlueDot/wwwroot/js/Admin/Agent/index.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(initializePage);
+
+function onAddSuccess() {
+    clean_modal();
+    initializePage();
+}
+
+function onDeleteSuccess() {
+    onAjaxSuccess();
+    $("#" + DeleteRowId).remove();
+}
+
+function initializePage() {
+    initializeDataTable($("#dataTables-table"));
+
+    $("tr").each(function (index, element) { $(element).attr("id", index); });
+
+    $('#form1').formValidation({
+        framework: 'bootstrap',
+        icon: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            UserName: {
+                validators: {
+                    notEmpty: {
+                        message: 'Por favor escriba el correo'
+                    },
+                }
+            }
+        }
+    });
+}
