8576165c0fb23849ba61ca9f3c86299f9cbf300e
Rafael A. Fernandez Morera <rfmorera@gmail.com>
2019-08-27 21:43:39 -0400

    partial

---
+++ Services/DTOs/AgentDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.DTOs
+{
+    public class AgentDTO
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ Services/Impls/AgentService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using BlueDot.Data.UnitsOfWorkInterfaces;
+using Microsoft.AspNetCore.Identity;
+using Services.DTOs;
+
+namespace Services.Impls
+{
+    public class AgentService : IAgentService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<IdentityUser> _userManager;
+        public AgentService(UserManager<IdentityUser> userManager, IUnitOfWork unitOfWork)
+        {
+            _userManager = userManager;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IdentityResult> AddAgent(AgentDTO dto)
+        {
+            IdentityUser agent = new IdentityUser(dto.UserName);
+            IdentityResult result = await _userManager.CreateAsync(agent);
+
+            if (!result.Succeeded) return result;
+            result = await _userManager.SetPhoneNumberAsync(agent, dto.Phone);
+
+            if (!result.Succeeded) return result;
+            result = await _userManager.SetEmailAsync(agent, dto.UserName);
+
+            return result;
+        }
+
+        public Task<AgentDetailsDTO> GetAgentDetails(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<AgentDTO>> GetAgents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAgent(string Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
