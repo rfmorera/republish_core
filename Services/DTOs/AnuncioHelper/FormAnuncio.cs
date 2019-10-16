61ead7d483c53ae5c8209e81da544aebf7af8765
Rafael Fernanadez <rfmorera@gmail.com>
2019-10-16 17:44:21 -0400

    JSon classes

---
+++ Services/DTOs/AnuncioHelper/FormAnuncio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.DTOs.AnuncioHelper
+{
+    class FormAnuncio
+    {
+        public FormAnuncio()
+        {
+            variables = new Variables();
+        }
+        public string operationName { get; } = "UpdateAdWithoutUser";
+        public Variables variables { get; set; }
+        public string query { get; } = "mutation UpdateAdWithoutUser($token: String!, $id: ID, $title: String, $description: String, $price: Float, $contactInfo: UpdateAdWithoutUserFormContactInfo!, $phone: String, $name: String, $subcategory: ID, $email: String, $captchaResponse: String!, $botScore: String, $images: [String]) {\n  updateAdWithoutUser(input: {token: $token, id: $id, title: $title, description: $description, price: $price, contactInfo: $contactInfo, phone: $phone, name: $name, subcategory: $subcategory, email: $email, captchaResponse: $captchaResponse, botScore: $botScore, images: $images}) {\n    ad {\n      id\n      title\n      description\n      price\n      phone\n      name\n      images {\n        edges {\n          node {\n            id\n            gcsKey\n            urls {\n              high\n              thumb\n              __typename\n            }\n            __typename\n          }\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    errors {\n      field\n      messages\n      __typename\n    }\n    clientMutationId\n    __typename\n  }\n}\n";
+    }
+}
---
+++ Services/DTOs/AnuncioHelper/Variables.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.DTOs.AnuncioHelper
+{
+    class Variables
+    {
+        public string token { get; set; }
+        public string id { get; set; }
+        public string subcategory { get; set; }
+        public string email { get; set; }
+        public string title { get; set; }
+        public string contactInfo { get; set; }
+        public string captchaResponse { get; set; }
+        public string botScore { get; set; }
+        public string[] images { get; set; }
+        public int price { get; set; }
+        public string description { get; set; }
+        public string name { get; set; }
+        public string phone { get; set; }
+    }
+}
