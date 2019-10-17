b476e192616682d157cbc93cb8de888ca424d6bc
Rafael Fernanadez <rfmorera@gmail.com>
2019-10-17 11:43:24 -0400

    Calcular costo mensual

--- Services/Extensions/TemporizadorExtension.cs
+++ Services/Extensions/TemporizadorExtension.cs
@@ -8,9 +8,9 @@ namespace Services.Extensions
 {
     public static class TemporizadorExtension
     {
-        public static bool IsValidDay(this Temporizador value)
+        public static bool IsValidDay(this Temporizador value, DateTime utcCuba)
         {
-            DayOfWeek today = DateTime.Now.ToUtcCuba().DayOfWeek;
+            DayOfWeek today = utcCuba.DayOfWeek;
             switch (today)
             {
                 case DayOfWeek.Sunday:
@@ -29,6 +29,36 @@ namespace Services.Extensions
                     return value.Sabado;
             }
             throw new Exception("Invalid day of week");
+        }
+
+        public static double Costo(this Temporizador value, double costoAnuncio, int cantidadAnuncios)
+        {
+            double val = 0;
+            int etapa = value.Etapa == 0 ? cantidadAnuncios : value.Etapa;
+            int intervaloMinutosTotal = value.IntervaloHoras * 60 + value.IntervaloMinutos;
+            TimeSpan time = value.HoraFin - value.HoraInicio;
+            int minutosEjecucion = (int) time.TotalMinutes;
+            int cantidadEjecuciones = minutosEjecucion / intervaloMinutosTotal;
+            double costoDiario = costoAnuncio * cantidadEjecuciones * etapa;
+
+            DateTime utcCuba = DateTime.Now.ToUtcCuba();
+            DateTime tmp = utcCuba;
+            utcCuba = utcCuba.AddDays(1 - utcCuba.Day);
+
+            for (int i = 0; i < 31; i++)
+            {
+                if (value.IsValidDay(tmp))
+                {
+                    val += costoDiario;
+                }
+                tmp = tmp.AddDays(1);
+                if(tmp.Month != utcCuba.Month)
+                {
+                    return val;
+                }
+            }
+
+            throw new Exception("Temporizador Costo> Month Never Change");
         }
     }
 }
