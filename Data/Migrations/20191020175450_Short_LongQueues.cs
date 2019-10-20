3df1e1b169017a6a38a01ed09677b20b8185db3f
Rafael Fernanadez <rfmorera@gmail.com>
2019-10-20 13:55:55 -0400

    Migration Short/Long Queues

---
+++ Data/Migrations/20191020175450_Short_LongQueues.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlueDot.Data.Migrations
+{
+    public partial class Short_LongQueues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LongQueue",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Url = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LongQueue", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ShortQueue",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Url = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShortQueue", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LongQueue");
+
+            migrationBuilder.DropTable(
+                name: "ShortQueue");
+        }
+    }
+}
