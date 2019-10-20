545644e50afcbd614e4cbe65d4fb729a939ba209
Rafael Fernanadez <rfmorera@gmail.com>
2019-10-20 17:12:38 -0400

    ShortQueeu Service created

---
+++ Services/IShortQueueService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public interface IShortQueueService
+    {
+        Task Process();
+        Task Clean();
+    }
+}
