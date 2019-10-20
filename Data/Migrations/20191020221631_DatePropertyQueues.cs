a1afe91694f971c2caf23bc32e76d86ad3b84eb4
Rafael Fernanadez <rfmorera@gmail.com>
2019-10-20 18:16:56 -0400

    DateProperty Queues migration

---
+++ Data/Migrations/20191020221631_DatePropertyQueues.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlueDot.Data.Migrations
+{
+    public partial class DatePropertyQueues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "ShortQueue",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "LongQueue",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "ShortQueue");
+
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "LongQueue");
+        }
+    }
+}
