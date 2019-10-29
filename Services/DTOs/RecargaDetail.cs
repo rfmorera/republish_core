611d239500e5e725673ea6618c59b910c76ed97a
Rafael Fernanadez <rfmorera@gmail.com>
2019-10-29 11:49:42 -0400

    RecargaDetailsDTO constructor added

--- Services/DTOs/RecargaDetail.cs
+++ Services/DTOs/RecargaDetail.cs
@@ -7,10 +7,18 @@ namespace Services.DTOs
 {
     public class RecargaDetail
     {
+        public RecargaDetail(string cliente, string agente, int monto, DateTime fecha)
+        {
+            Client = cliente;
+            Agente = agente;
+            Monto = monto;
+            Fecha = fecha;
+        }
+
         public int Monto { get; set; }
 
         public string Client { get; set; }
-
+        public string Agente { get; set; }
         public DateTime Fecha { get; set; }
         public string FechaAsString { get { return Fecha.ToDateString(); } }
     }
