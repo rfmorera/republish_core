4cd95115c0c0317f71d6f3f613aae2bb6bb939da
Rafael Fernanadez <rfmorera@gmail.com>
2019-10-29 10:30:11 -0400

    DTO updated

--- Services/DTOs/UserDTO.cs
+++ Services/DTOs/UserDTO.cs
@@ -7,16 +7,18 @@ namespace Services.DTOs
 {
     public class UserDTO
     {
-        public UserDTO(string Id, string userName, string email, string Role)
+        public UserDTO(string Id, string userName, string email, string Role, double saldo)
         {
             this.Id = Id;
             Username = userName;
             Email = email;
             this.Role = Role;
+            SaldoActual = Math.Round(saldo, 3);
         }
 
         public string Id { get; set; }
         public string Username { get; set; }
+        public double SaldoActual { get; set; }
         public string Email { get; set; }
         public string Role { get; set; }
     }
