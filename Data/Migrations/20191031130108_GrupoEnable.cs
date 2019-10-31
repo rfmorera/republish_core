d37e572d056d677fb69a9437fa687e76a46ec8b6
Rafael Fernanadez <rfmorera@gmail.com>
2019-10-31 10:09:28 -0400

    DataMigrations grupoenable

---
+++ Data/Migrations/20191031130108_GrupoEnable.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlueDot.Data.Migrations
+{
+    public partial class GrupoEnable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "GrupoEnable",
+                table: "Temporizador",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Enable",
+                table: "Grupo",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GrupoEnable",
+                table: "Temporizador");
+
+            migrationBuilder.DropColumn(
+                name: "Enable",
+                table: "Grupo");
+        }
+    }
+}
