3f18b99c07f49545c395c1d267cc6ef12b71383c
Rafael Fernanadez <rfmorera@gmail.com>
2019-11-01 01:35:15 -0400

    DTO title added

--- Services/DTOs/AnuncioDTO.cs
+++ Services/DTOs/AnuncioDTO.cs
@@ -12,10 +12,12 @@ namespace Services.DTOs
             Id = a.Id;
             Url = a.Url;
             Caducado = a.Actualizado;
+            Title = a.Titulo ?? string.Empty;
         }
 
         public string Id { get; set; }
         public string Url { get; set; }
+        public string Title { get; set; }
         public bool Caducado { get; set; }
     }
 }
