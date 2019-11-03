d73af2c80a720d429ba0d253160b4ef02dbd5ada
Rafael Fernanadez <rfmorera@gmail.com>
2019-11-03 09:28:57 -0500

    Notification service completed

---
+++ Services/INotificationsService.cs
@@ -0,0 +1,14 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public interface INotificationsService
+    {
+        Task<IEnumerable<Notificacion>> GetByUser(string Id);
+        Task SetReadedByUser(string Id);
+    }
+}
---
+++ Services/Impls/NotificationsService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using Republish.Data;
+using Republish.Data.Repositories;
+using Republish.Data.RepositoriesInterfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Services.Impls
+{
+    public class NotificationsService : INotificationsService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IRepository<Notificacion> repositoryNotificacion;
+
+        public NotificationsService(ApplicationDbContext context)
+        {
+            _context = context;
+            repositoryNotificacion = new Repository<Notificacion>(context);
+        }
+
+        public async Task<IEnumerable<Notificacion>> GetByUser(string Id)
+        {
+            return await repositoryNotificacion.QueryAll()
+                                           .Where(n => n.UserId == Id)
+                                           .OrderByDescending(n => n.DateCreated)
+                                           .ToListAsync();
+        }
+
+        public async Task SetReadedByUser(string Id)
+        {
+            IEnumerable<Notificacion>list = await repositoryNotificacion.QueryAll()
+                                           .Where(n => n.UserId == Id && n.Readed == false)
+                                           .OrderByDescending(n => n.DateCreated)
+                                           .ToListAsync();
+
+            foreach(Notificacion item in list)
+            {
+                item.Readed = true;
+                await repositoryNotificacion.UpdateAsync(item, item.Id);
+            }
+            await repositoryNotificacion.SaveChangesAsync();
+        }
+    }
+}
