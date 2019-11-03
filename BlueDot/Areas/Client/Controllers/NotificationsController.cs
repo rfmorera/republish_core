1a0c6c650c5f6330da9b3df4eb50e19e10a32749
Rafael Fernanadez <rfmorera@gmail.com>
2019-11-03 09:29:14 -0500

    View and Controller ready

---
+++ BlueDot/Areas/Client/Controllers/NotificationsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Models;
+using Services;
+
+namespace RepublishTool.Areas.Client.Controllers
+{
+    [Area("Client")]
+    [Authorize(Roles = "Client")]
+    public class NotificationsController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly INotificationsService _notificationsService;
+        
+        public NotificationsController(UserManager<IdentityUser> userManager, INotificationsService notificationsService)
+        {
+            _userManager = userManager;
+            _notificationsService = notificationsService;
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            IdentityUser user = await _userManager.GetUserAsync(HttpContext.User);
+            IEnumerable<Notificacion> notificacions = await _notificationsService.GetByUser(user.Id);
+            await _notificationsService.SetReadedByUser(user.Id);
+            return View(notificacions);
+        }
+    }
+}
