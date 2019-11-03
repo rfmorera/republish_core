31867bd6b99c39cc9744e3a2bb1eb13ca58f0c1c
Rafael Fernanadez <rfmorera@gmail.com>
2019-11-03 08:40:37 -0500

    Migration created

---
+++ Data/Migrations/20191103134014_CreateNotificatinos.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlueDot.Data.Migrations
+{
+    public partial class CreateNotificatinos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notificacion",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    UserId = table.Column<string>(nullable: false),
+                    Mensaje = table.Column<string>(nullable: true),
+                    Readed = table.Column<bool>(nullable: false),
+                    DateCreated = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notificacion", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Notificacion_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notificacion_UserId",
+                table: "Notificacion",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notificacion");
+        }
+    }
+}
