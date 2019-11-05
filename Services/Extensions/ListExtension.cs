9e01e6da1ff09de459c89cff42e0d5d4c614a6f6
Rafael Fernanadez <rfmorera@gmail.com>
2019-11-05 00:04:07 -0500

    DTO extension

---
+++ Services/Extensions/ListExtension.cs
@@ -0,0 +1,21 @@
+﻿using Models;
+using Services.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Extensions
+{
+    public static class ListExtension
+    {
+        public static IEnumerable<NotificacionDTO> ToNotificacionDTO(this IEnumerable<Notificacion> source)
+        {
+            List<NotificacionDTO> list = new List<NotificacionDTO>();
+            foreach(Notificacion item in source)
+            {
+                list.Add(new NotificacionDTO(item));
+            }
+            return list;
+        }
+    }
+}
