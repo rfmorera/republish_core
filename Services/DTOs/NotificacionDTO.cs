1f3623fb7d5e675df16ac8c9aff0d92ba31e8258
Rafael Fernanadez <rfmorera@gmail.com>
2019-11-05 00:04:01 -0500

    Notification DTO

---
+++ Services/DTOs/NotificacionDTO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Models;
+
+namespace Services.DTOs
+{
+    public class NotificacionDTO
+    {
+        public NotificacionDTO(Notificacion item)
+        {
+            Id = item.Id;
+            Mensaje = item.Mensaje;
+            Readed = item.Readed;
+            DateCreated = item.DateCreated;
+        }
+
+        public string Id { get; set; }
+
+        public string Mensaje { get; set; }
+        public bool Readed { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string Status
+        {
+            get
+            {
+                if (Readed) return "alert-warning";
+                return "alert-success";
+            }
+        }
+    }
+}
