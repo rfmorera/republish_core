6135d11d4f507f7f442215c6ca7c6557e57f2cb5
Rafael Fernanadez <rfmorera@gmail.com>
2019-11-07 00:09:11 -0500

    anuncio title migration

---
+++ Data/Migrations/20191107043140_AnuncioTitulo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlueDot.Data.Migrations
+{
+    public partial class AnuncioTitulo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Titulo",
+                table: "Anuncio",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Titulo",
+                table: "Anuncio");
+        }
+    }
+}
