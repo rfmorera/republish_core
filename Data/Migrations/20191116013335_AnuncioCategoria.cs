fb40d9d4257a87ca66a983df285acd556cac3e09
Rafael Fernanadez <rfmorera@gmail.com>
2019-11-15 20:33:58 -0500

    Anuncio Categoria migrations

---
+++ Data/Migrations/20191116013335_AnuncioCategoria.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlueDot.Data.Migrations
+{
+    public partial class AnuncioCategoria : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Categoria",
+                table: "Anuncio",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Enable",
+                table: "Anuncio",
+                nullable: false,
+                defaultValue: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Categoria",
+                table: "Anuncio");
+
+            migrationBuilder.DropColumn(
+                name: "Enable",
+                table: "Anuncio");
+        }
+    }
+}
