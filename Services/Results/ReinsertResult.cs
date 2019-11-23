6cca5fa2dd3c7f9bd6e5697b70b85105f9b6a12c
Rafael Fernanadez <rfmorera@gmail.com>
2019-11-22 23:05:49 -0500

    ReinsertResult

---
+++ Services/Results/ReinsertResult.cs
@@ -0,0 +1,17 @@
+﻿using Models;
+using Services.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Results
+{
+    public class ReinsertResult
+    {
+        public Anuncio Anuncio { get; set; }
+        public bool HasException { get; set; }
+        public BaseException Exception { get; set; }
+        public bool Success { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
