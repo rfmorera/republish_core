25dd4095b49f914917497054572784ea1bddc49e
Rafael Fernanadez <rfmorera@gmail.com>
2019-11-23 15:26:05 -0500

    Operations Resuts

---
+++ Services/Results/InsertResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Results
+{
+    public class InsertResult
+    {
+        public InsertResult(string id, string token)
+        {
+            Id = id;
+            Token = token;
+        }
+
+        public string Id { get; set; }
+        public string Token { get; set; }
+        public string FullId
+        {
+            get
+            {
+                return Token + Id;
+            }
+        }
+    }
+}
--- Services/Results/ReinsertResult.cs
+++ Services/Results/ReinsertResult.cs
@@ -8,9 +8,28 @@ namespace Services.Results
 {
     public class ReinsertResult
     {
+        public ReinsertResult(Anuncio anuncio)
+        {
+            Anuncio = anuncio;
+            Success = true;
+        }
+
+        public ReinsertResult(Anuncio anuncio, Exception ex)
+        {
+            Anuncio = anuncio;
+            Exception = ex;
+            Success = false;
+        }
+
         public Anuncio Anuncio { get; set; }
-        public bool HasException { get; set; }
-        public BaseException Exception { get; set; }
+        public bool HasException
+        {
+            get
+            {
+                return Exception != null;
+            }
+        }
+        public Exception Exception { get; set; }
         public bool Success { get; set; }
         public bool IsDeleted { get; set; }
     }
