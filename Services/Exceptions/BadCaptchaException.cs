6b5e0dee64153ba4933f5109f59d4d1782d1953c
Rafael Fernanadez <rfmorera@gmail.com>
2019-11-22 23:04:19 -0500

    Refactorizing Exceptions

--- Services/Exceptions/BadCaptchaException.cs
+++ Services/Exceptions/BadCaptchaException.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Services.Exceptions
 {
-    public class BadCaptchaException : Exception
+    public class BadCaptchaException : BaseException
     {
         public string uri { get; set; }
         public BadCaptchaException(string message, string _uri) : base(message)
