9386c16555dcc5aabf21680b43fdfa3f432ee674
Rafael Fernanadez <rfmorera@gmail.com>
2019-12-05 12:22:47 -0500

    Cannot Delete Exception added

---
+++ Services/Exceptions/CannotDeleteRecord.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Exceptions
+{
+    public class CannotDeleteRecord : Exception
+    {
+        public CannotDeleteRecord(string message) : base(message)
+        {
+
+        }
+    }
+}
