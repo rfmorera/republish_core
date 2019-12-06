69b1c98bb6bda48d4ffffdaddc3b05c1955b3f14
Rafael Fernanadez <rfmorera@gmail.com>
2019-12-06 17:01:39 -0500

    Migration

---
+++ Data/Migrations/20191206215933_AnuncioData.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlueDot.Data.Migrations
+{
+    public partial class AnuncioData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "Enable",
+                table: "Anuncio",
+                nullable: true,
+                oldClrType: typeof(bool));
+
+            migrationBuilder.AddColumn<string>(
+                name: "FormUpdateAnuncio",
+                table: "Anuncio",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FormUpdateAnuncio",
+                table: "Anuncio");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Enable",
+                table: "Anuncio",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldNullable: true);
+        }
+    }
+}
